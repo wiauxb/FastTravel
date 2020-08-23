049e43b8d03c7a94b8ac93dbc64358bbecce2999
wiauxb <bastien.wiaux@student.uclouvain.be>
2020-08-23 11:30:17 +0200

    added some ideas/ bug report

--- src/main/java/net/fast/travel/FastTravel.java
+++ src/main/java/net/fast/travel/FastTravel.java
@@ -29,8 +29,11 @@ public class FastTravel implements ModInitializer {
  	V lier un teleporteur dans un chunk non chargé ne sauve pas son tag
  	V systême de selection doit être étendu au multijoueur
  	V Chunk chargé par la selection doit être déchargé et la selection effacée à la déconnexion
- 	- teleport à la position relative au teleporter
+ 	o teleport à la position relative au teleporter
  	V le passage end -> overworld ne teleport pas au bon endroit car le passage à l'overworld n'est pas fini lorsqu'on tp aux bonnes positions
  	V probleme avec les ticking entity: lié au passage nether, si le joueur n'a jamais utilisé de netherportal NullPointer exception ServerPlayerEntity.java:702 (lié à moveToWorld, ServerPlayerEntity.java:613, Entity.java:2222)
     V les items disparaisses si le teleporter est dans un chunk non chargé
+    - joueur sort du minecart coté client mais pas coté server: -> bug général pour rideable
+    - minecart ne tp qu'une fois
+    o teleportation detectable par observer
  */
